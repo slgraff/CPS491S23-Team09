451ca3f00c09b28d3153bd10af00cbeba5607c7a
Amanda Bolden <40702909+Mastery01@users.noreply.github.com>
2023-03-03 03:20:09 +0000

    web server set up using flask import and python

--- WebServer/App.py
+++ WebServer/App.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+import socketio
 import subprocess
+
+sio = socketio.Server() #creating socketio server instance 
+
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -9,18 +13,30 @@ socketio = SocketIO(app)
 def index() -> str:
     return render_template('iRoombaInterface.html')
 
-@socketio.on('connect')
-def handle_connect():
-    print('Client connected')
+@socketio.on('connect') #event handler when client connects to server the connect function is called and logs message to console.
+def connect(sid, environ):
+    print('Client connected', sid)
 
 @socketio.on('disconnect')
-def handle_disconnect():
+def disconnect():
     print('Client disconnected')
 
-@socketio.on('start_program')
-def handle_start_program():
+
+@socketio.on('start_program') #when event is emiited from web interface this function is called which start tetheredDrive.py using the subporcess module
+def start_program(sid):
+    print('Starting TetheredDrive.py')
+
+    import os # possiblyremove later 
+    directory = "logs" # possiblyremove later 
+
+    # create logs directory if does not exist
+    if not os.path.exists(directory):# possiblyremove later 
+        os.makedirs(directory) # possiblyremove later 
     # Start the program here
-    subprocess.Popen(['python', '/path/to/iroomba.py'])
+    subprocess.Popen(['python', '/workspaces/CPS491S23-Team09/Create2/TetheredDrive'])
+
+app = socketio.WSGIApp(sio)
 
 if __name__ == '__main__':
-    socketio.run(app)
+    #socketio.run(app) possibly remove switch to lower line 
+    app.run()

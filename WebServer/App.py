64e1356230638a9f41252466e3670ddd2195b1ab
Amanda Bolden <40702909+Mastery01@users.noreply.github.com>
2023-03-09 03:59:50 +0000

    updated app.py code, socketio to run webserver loc

--- WebServer/App.py
+++ WebServer/App.py
@@ -1,42 +1,55 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
-import socketio
+from flask_socketio import SocketIO
 import subprocess
-
-sio = socketio.Server() #creating socketio server instance 
-
+import os
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+
 
 @app.route('/')
 def index() -> str:
     return render_template('iRoombaInterface.html')
 
-@socketio.on('connect') #event handler when client connects to server the connect function is called and logs message to console.
-def connect(sid, environ):
+
+# event handler when client connects to server the connect function is called and logs message to console.
+@socketio.on('connect')
+def connect(sid):
+    """
+    Event Handler called when client connects to the server. 
+    :param sid: The session ID for client 
+    """
     print('Client connected', sid)
+
 
 @socketio.on('disconnect')
 def disconnect():
+    """
+    Event handler called when a client disconnects from the server.
+    """
     print('Client disconnected')
 
 
-@socketio.on('start_program') #when event is emiited from web interface this function is called which start tetheredDrive.py using the subporcess module
-def start_program(sid):
+# when event is emitted from web interface this function is called which start tetheredDrive.py using the subporcess module
+@socketio.on('start_program')
+def start_program():
+    """
+    Event handler called when the 'start_program' event is emitted from the web interface.
+    Starts the TetheredDrive.py program using the subprocess module.
+    """
     print('Starting TetheredDrive.py')
 
-    import os # possiblyremove later 
-    directory = "logs" # possiblyremove later 
+    directory = "logs"  # possiblyremove later
 
     # create logs directory if does not exist
-    if not os.path.exists(directory):# possiblyremove later 
-        os.makedirs(directory) # possiblyremove later 
+    if not os.path.exists(directory):  # possiblyremove later
+        os.makedirs(directory)  # possiblyremove later
     # Start the program here
-    subprocess.Popen(['python', '/workspaces/CPS491S23-Team09/Create2/TetheredDrive'])
+    subprocess.Popen(
+        ['python', '/workspaces/CPS491S23-Team09/Create2/TetheredDrive.py'])
 
-app = socketio.WSGIApp(sio)
 
 if __name__ == '__main__':
-    #socketio.run(app) possibly remove switch to lower line 
+    socketio.init_app(app)  # possibly remove switch to lower line
     app.run()
+    
